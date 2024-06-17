9f77964600c280342b1536a87640da809dc31136
GVenkatachalapathi <122212678+GVenkatachalapathi@users.noreply.github.com>
2024-06-17 17:06:23 +0530

    Add files via upload

---
+++ dwdm-2-7.R
@@ -0,0 +1,10 @@
+data(mtcars)
+mortality <- mtcars$mpg
+hardness <- mtcars$hp
+plot(hardness, mortality, main = "Linear Regression: Mortality vs. Hardness",
+     xlab = "Hardness", ylab = "Mortality", pch = 16, col = "blue")
+linear_model <- lm(mortality ~ hardness)
+abline(linear_model, col = "red")
+new_data <- data.frame(hardness = 88)
+predicted_mortality <- predict(linear_model, newdata = new_data)
+cat("Predicted Mortality for Hardness=88:", predicted_mortality, "
